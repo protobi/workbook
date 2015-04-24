c5c584e86fc9e1281b7cf03b19e13eb6c78c5fbc
Pieter Sheth-Voss <pieter@protobi.com>
2015-04-23 23:10:40 -0400

    Remove console.log call

--- workbook.js
+++ workbook.js
@@ -1,7 +1,6 @@
 // Support  CommonJS, AMD or independent user
 (function (name, definition) {
   if (typeof define == 'function' && typeof define.amd == 'object') {
-    console.log("AMD");
     define(definition);
   }
   else if (typeof module != 'undefined') module.exports = definition();
@@ -107,7 +106,6 @@
               if (typeof cell.v === 'number') cell.t = 'n';
               else if (typeof cell.v === 'boolean') cell.t = 'b';
               else if (cell.v instanceof Date) {
-                console.log("Converting")
                 cell.t = 'n';
                 cell.z = XLSX.SSF._table[14];
                 cell.v = this.datenum(cell.v);
